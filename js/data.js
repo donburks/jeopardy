296f13e5486d44b25832789c66b15d082e34e64e
Don Burks <don@donburks.com>
2016-09-12 16:43:58 -0700

    Initial commit

---
+++ js/data.js
@@ -0,0 +1,17 @@
+var data = {
+  categories: ["Acronyms", "Network Terms", "Web Stack", "Coding Languages", "Databases"],
+  questions: {
+    acronyms: ["HTTP", "DNS", "CSS", "IP, as in IP Address", "AJAX"],
+    network: ["This term is for when transmission over a network connection is slow. Also known as 'lag'", "This device allows multiple computers to share the same IP Address", "This is the name for a type of company which provides connectivity to homes and businesses.", "This is the term for a piece of software that protects your network against incoming attacks.", "More commonly known as WiFi, this is the actual number of the protocol for wireless internet."],
+    web: ["This framework is based in Ruby, and is very popular with startups. Also, Shopify and GitHub.", "Known as the LAMP stack, it consists of these four popular components.", "It's always 'nice' to use this ____ stack, which includes Angular and MongoDB.", "This framework shares the name with the main character from a Quentin Tarantino movie. Also, written in Python.", "This front-end framework, written by Facebook, is extremely popular since it came out just over a year ago."],
+    coding: ["Originally written just for the browser, this language can now be used  on the server side, using Node.", "Used by products like WordPress and Facebook, this is one of the most popular web programming languages of all time.", "Written to make coding enjoyable, this language is named after a jewel.", "Now owned by Oracle, a lot of business software is written in this coffee-themed language.", "Originally written in 1978, this language is still used today to build software and operating systems such as Unix and Linux."],
+    databases: ["Written as the sequel to QUEL, it is now the standard for databases.", "This database is named after a piece of furniture.", "This database can be used on mobile phones.", "This category of databases doesn't store its data in tables, instead it stores as documents.", "This type of database enables Six Degrees of Separation queries."] 
+  },
+  answers: {
+    acronyms: ["Hypertext Transfer Protocol", "Domain Name System", "Cascading Style Sheets", "Internet Protocol", "Asynchronous JavaScript and XML"],
+    network: ["Latency", "Router", "ISP", "Firewall", "802.11"],
+    web: ["Ruby on Rails", "Linux, Apache, MySQL, and PHP", "MEAN - MongoDB, Express, Angular, and Node", "Django", "React"],
+    coding: ["JavaScript", "PHP", "Ruby", "Java", "C"],
+    databases: ["SQL", "CouchDB", "SQLite", "NoSQL", "Graph"]
+  }
+};
---
+++ js/jeopardy.js
@@ -0,0 +1,55 @@
+$(function() {
+  function getQuestion(cat, i) {
+    return data.questions[cat][i]; 
+  }
+
+  function getAnswer(cat, i) {
+    return data.answers[cat][i];
+  }
+
+  function showQuestion() {
+    $("#board").fadeOut('fast', function() {
+      $("#question").fadeIn('fast');
+    }); 
+  }
+
+  function showBoard() {
+    $("#question").fadeOut('fast', function() {
+      $("#board").fadeIn('fast');
+    });
+  }
+
+  function showAnswer() {
+    $("#questionText").fadeOut('fast', function() {
+      $(this).text($(this).data('answer')).fadeIn('fast');
+    });
+  }
+
+  data.categories.forEach(function(category, idx) {
+    $("<div>").addClass('cell col one sixth').text(category.toUpperCase()).appendTo("header.row");
+    var cat = category.toLowerCase().split(' ')[0];
+    var column = $("<div>").addClass("col one sixth").appendTo("#board section.row");
+    [100, 200, 300, 400, 500].forEach(function(value, idx) {
+      $("<div>").addClass('cell question').text("$"+value).data('category', cat).data('index', idx).appendTo(column);
+    });
+  });
+ 
+  $(".cell.question").on('click', function() {
+    var category = $(this).data('category');
+    var index = $(this).data('index');
+    $(this).text('').off('click').css('cursor', 'normal');
+
+    $("#questionText").text(getQuestion(category, index)).data('answer', getAnswer(category, index));
+    showQuestion();
+  }); 
+
+  $("#question").on('click', showAnswer);
+
+  $("body").on('keyup', function(evt) {
+    if (evt.keyCode == 13 && $("#question").is(":visible")) {
+      showBoard();
+    } else {
+      return false;
+    }
+  });
+});
